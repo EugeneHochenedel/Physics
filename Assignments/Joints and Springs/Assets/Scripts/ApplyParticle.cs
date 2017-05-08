0593990437de10b65b12e8bdce5c1890a73abe73
Eugene Hochenedel <eugeneatschool@gmail.com>
2017-05-08 14:40:46 -0500

    Added Build for Cloth Simulation

--- Assignments/Joints and Springs/Assets/Scripts/ApplyParticle.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ApplyParticle : MonoBehaviour
-{
-	public Particle particle;
-	public SpringDamper springdamper;
-	public Triangle triangle;
-}
